74efb97486e51a1e3d88ddf075c86ef188c85c01
miretz <1217795+Miretz@users.noreply.github.com>
2021-11-27 17:26:10 +0100

    restructued the project to enable unit testing using googletest

---
+++ test/utils_test.cpp
@@ -0,0 +1,18 @@
+#include "utils.hpp"
+
+#include "gtest/gtest.h"
+
+TEST(Utils, trim)
+{
+    EXPECT_STREQ("hello", Utils::trim("  hello   ").c_str());
+}
+
+TEST(Utils, addPlus_valid)
+{
+    EXPECT_STREQ("+4", Utils::addPlus(4).c_str());
+}
+
+TEST(Utils, addPlus_zero)
+{
+    EXPECT_STREQ(" 0", Utils::addPlus(0).c_str());
+}
